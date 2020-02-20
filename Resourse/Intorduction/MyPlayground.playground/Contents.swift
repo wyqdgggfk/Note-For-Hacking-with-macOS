9a4184b2b540458db7696b6a989ae01b683b5ad1
Jason Wang <wyqdgggfk@aol.com>
2020-02-20 23:55:15 +0800

    Update Contents.swift

--- Resourse/Intorduction/MyPlayground.playground/Contents.swift
+++ Resourse/Intorduction/MyPlayground.playground/Contents.swift
@@ -3,3 +3,5 @@ var name = "Micheal" //This is a variable,means can be changed
 name = "Jack" //Change name from Micheal to Jack
 let anotherName = "Tomi"//Can't change anotherName's value
 
+var age:Int
+age = 20
