ecb528deb1c807930e915ce7c8462dec06419074
Jason Wang <wyqdgggfk@aol.com>
2020-02-22 15:54:06 +0800

    Update SourceViewController.swift

--- Resourse/Project01StormView/Project01StormView/SourceViewController.swift
+++ Resourse/Project01StormView/Project01StormView/SourceViewController.swift
@@ -10,6 +10,7 @@ import Cocoa
 
 class SourceViewController: NSViewController {
 
+    @IBOutlet var tableView: NSTableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
