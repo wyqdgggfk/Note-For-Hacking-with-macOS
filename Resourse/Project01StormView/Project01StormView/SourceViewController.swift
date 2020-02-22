adb38f7aee28a78ccc0fa87f6cd65e0e71762b6a
Jason Wang <wyqdgggfk@aol.com>
2020-02-22 17:50:48 +0800

    Update SourceViewController.swift

--- Resourse/Project01StormView/Project01StormView/SourceViewController.swift
+++ Resourse/Project01StormView/Project01StormView/SourceViewController.swift
@@ -8,12 +8,11 @@
 
 import Cocoa
 
-class SourceViewController: NSViewController,NSTableViewDataSource,NSTableViewDelegate {
+class SourceViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
 
     @IBOutlet var tableView: NSTableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
     }
     func numberOfRows(in tableView: NSTableView) -> Int {
         return 100
