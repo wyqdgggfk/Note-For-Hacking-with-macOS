a6e04c23a9324fd0287cd2da052ac84317efdafe
Jason Wang <wyqdgggfk@aol.com>
2020-02-22 13:02:20 +0800

    Create SourceViewController.swift

---
+++ Resourse/Project01StormView/Project01StormView/SourceViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SourceViewController.swift
+//  Project01StormView
+//
+//  Created by Jason on 2020/2/22.
+//  Copyright © 2020 jiangrenandu. All rights reserved.
+//
+
+import Cocoa
+
+class SourceViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
