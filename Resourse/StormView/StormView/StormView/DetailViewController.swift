1d2a5bc05d6fe64b56ecec9f4c7972ac6df3d826
Jason Wang <wyqdgggfk@aol.com>
2020-02-23 21:11:04 +0800

    Create DetailViewController.swift

---
+++ Resourse/StormView/StormView/StormView/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  StormView
+//
+//  Created by Jason on 2020/2/23.
+//  Copyright © 2020 jiangrenandu. All rights reserved.
+//
+
+import Cocoa
+
+class DetailViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
