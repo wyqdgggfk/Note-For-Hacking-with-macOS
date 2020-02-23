e5a950e31a3f517d367408694829cbabbf6fbdbc
Jason Wang <wyqdgggfk@aol.com>
2020-02-23 16:24:54 +0800

    Create ViewController.swift

---
+++ Resourse/StormView/StormView/StormView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StormView
+//
+//  Created by Jason on 2020/2/23.
+//  Copyright © 2020 jiangrenandu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
