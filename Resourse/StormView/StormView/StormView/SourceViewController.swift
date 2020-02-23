d7afff88e2098125e7e2c24392f7e0c56caecb3b
Jason Wang <wyqdgggfk@aol.com>
2020-02-24 00:01:56 +0800

    Update SourceViewController.swift

--- Resourse/StormView/StormView/StormView/SourceViewController.swift
+++ Resourse/StormView/StormView/StormView/SourceViewController.swift
@@ -8,11 +8,23 @@
 
 import Cocoa
 
-class SourceViewController: NSViewController {
-
+class SourceViewController: NSViewController,NSTableViewDelegate,NSTableViewDataSource {
+    @IBOutlet var tableView: NSTableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return 400
+    }
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else {
+            return nil
+        }
+        vw.textField?.stringValue = "he"
+        return vw
+    }
 }
