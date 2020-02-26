6ddd4775d31d86e2b726b856a145ed11a050db21
Jason Wang <wyqdgggfk@aol.com>
2020-02-26 21:39:08 +0800

    Create Project 01

---
+++ Resourse/Project01/Project01/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Project01
+//
+//  Created by Jason on 2020/2/26.
+//  Copyright © 2020 jiangrenandu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
