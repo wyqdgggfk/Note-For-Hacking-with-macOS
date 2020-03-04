a4c3d60cf7fe858a930c4c7c391e03cda0520abd
Jason Wang <wyqdgggfk@aol.com>
2020-03-04 23:54:47 +0800

    Update SourceViewController.swift

--- Resourse/Project01/Project01/SourceViewController.swift
+++ Resourse/Project01/Project01/SourceViewController.swift
@@ -15,7 +15,17 @@ class SourceViewController: NSViewController,NSTableViewDelegate,NSTableViewData
         super.viewDidLoad()
         // Do view setup here.
     }
+    // 确定左侧列表有多少行
     func numberOfRows(in tableView: NSTableView) -> Int {
         return 100
+    }
+    
+    // 确定左侧列表每行显示的内容
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else {
+            return nil
+        }
+        vw.textField?.stringValue = "Hello World!"
+        return vw
     }
 }
