108b6c261380f95e3d1863391ce350a309ab0382
Jason Wang <wyqdgggfk@aol.com>
2020-03-05 23:41:28 +0800

    Update SourceViewController.swift

--- Resourse/Project01/Project01/SourceViewController.swift
+++ Resourse/Project01/Project01/SourceViewController.swift
@@ -10,14 +10,25 @@ import Cocoa
 
 class SourceViewController: NSViewController,NSTableViewDelegate,NSTableViewDataSource {
     @IBOutlet var tableView: NSTableView!
+    var applications = [String]() //创建一个 pictures 数组储存照片的名字
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        let fm = FileManager.default
+        let path = "/Applications"
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        for item in items {
+            if item.hasSuffix(".app"){
+                applications.append(item.replacingOccurrences(of: ".app", with: ""))
+            }
+            
+        }
+        
+        
     }
     // 确定左侧列表有多少行
     func numberOfRows(in tableView: NSTableView) -> Int {
-        return 100
+        return applications.count
     }
     
     // 确定左侧列表每行显示的内容
@@ -25,7 +36,7 @@ class SourceViewController: NSViewController,NSTableViewDelegate,NSTableViewData
         guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else {
             return nil
         }
-        vw.textField?.stringValue = "Hello World!"
+        vw.textField?.stringValue = applications[row]
         return vw
     }
 }
