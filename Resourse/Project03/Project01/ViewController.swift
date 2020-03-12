d7cc7d05d5afbc5b916d9d0fc15513246cc32c55
Jason Wang <wyqdgggfk@aol.com>
2020-03-12 14:54:23 +0800

    create toolbar button

--- Resourse/Project03/Project01/ViewController.swift
+++ Resourse/Project03/Project01/ViewController.swift
@@ -8,13 +8,18 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSSplitViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func sharedClicked(_ sender: NSView){
+        guard let detail = children[1] as? DetailViewController else {return}
+        guard let image = detail.imageView.image else {return}
+        let picker = NSSharingServicePicker(items: [image])
+        picker.show(relativeTo: .zero, of: sender, preferredEdge: .minY)
+    }
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
