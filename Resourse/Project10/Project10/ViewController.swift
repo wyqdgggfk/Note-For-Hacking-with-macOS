e73b8dce757ded1433fff71029b2435c24747059
Jason Wang <wyqdgggfk@aol.com>
2020-03-19 23:41:32 +0800

    create Project10

---
+++ Resourse/Project10/Project10/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Jason on 2020/3/19.
+//  Copyright © 2020 jiangrenandu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
