9eab319614a4f551553296a9f4138d2688dba0cc
Jason Wang <wyqdgggfk@aol.com>
2020-03-23 11:57:03 +0800

    Update ViewController.swift

--- Resourse/Project10/Project10/ViewController.swift
+++ Resourse/Project10/Project10/ViewController.swift
@@ -7,15 +7,22 @@
 //
 
 import Cocoa
+import MapKit
 
 class ViewController: NSViewController {
-
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var apiKey: NSTextField!
+    @IBOutlet var statusBarOption: NSPopUpButton!
+    @IBOutlet var units: NSSegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func showPoweredBy(_ sender: NSButton) {
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
