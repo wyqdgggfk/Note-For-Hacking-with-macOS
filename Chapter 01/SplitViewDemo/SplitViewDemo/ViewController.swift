0576b8330dca847e9f0d322e3c3261cbfc75983b
Jason Wang <wyqdgggfk@aol.com>
2020-03-28 19:38:14 +0800

    add notes

--- Chapter 01/SplitViewDemo/SplitViewDemo/ViewController.swift
+++ Chapter 01/SplitViewDemo/SplitViewDemo/ViewController.swift
@@ -6,6 +6,10 @@
 //  Copyright © 2020 jiangrenandu. All rights reserved.
 //
 
+// This application is just a demo for learning
+// This application will does nothing but shows an appearance to users to choose language
+// This application should contain a plist file to store diffrent languages
+
 import Cocoa
 
 class ViewController: NSViewController {
