5d17eaedbeedf0e697c9fabbcdd1cd34b2b29667
Jason Wang <wyqdgggfk@aol.com>
2020-03-28 20:15:06 +0800

    add two viewcontroller.swift

---
+++ Chapter 01/SplitViewDemo/SplitViewDemo/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  SplitViewDemo
+//
+//  Created by Jason on 2020/3/28.
+//  Copyright © 2020 jiangrenandu. All rights reserved.
+//
+
+import Cocoa
+
+class DetailViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
