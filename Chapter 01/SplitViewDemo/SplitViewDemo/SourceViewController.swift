f87fcbf6bde5fed58b17c2fc0bc8d84f6018712c
Jason Wang <wyqdgggfk@aol.com>
2020-03-29 15:34:46 +0800

    Update SourceViewController.swift

--- Chapter 01/SplitViewDemo/SplitViewDemo/SourceViewController.swift
+++ Chapter 01/SplitViewDemo/SplitViewDemo/SourceViewController.swift
@@ -10,16 +10,17 @@ import Cocoa
 
 class SourceViewController: NSViewController,NSTableViewDataSource,NSTableViewDelegate {
     
-    let languageList: [String] = ["简体中文","繁體中文","English","Deutsch"," русский язык"]
+    let languageList: [String] = ["简体中文","繁體中文","English","Deutsch"," русский язык","Le français"]
    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    // how many rows will show on the left
     func numberOfRows(in tableView: NSTableView) -> Int {
         return languageList.count
     }
-    
+//    what will show on the right
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
         guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else {
             return nil
