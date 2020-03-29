850b5af5bcb7b4859c23599ff665516b20dca5f3
Jason Wang <wyqdgggfk@aol.com>
2020-03-29 20:38:00 +0800

    Update DetailViewController.swift

--- Chapter 01/SplitViewDemo/SplitViewDemo/DetailViewController.swift
+++ Chapter 01/SplitViewDemo/SplitViewDemo/DetailViewController.swift
@@ -9,11 +9,14 @@
 import Cocoa
 
 class DetailViewController: NSViewController {
+    @IBOutlet var labelText: NSTextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-    
+    func labelChange(country: String) {
+        labelText.stringValue = country
+    }
 }
