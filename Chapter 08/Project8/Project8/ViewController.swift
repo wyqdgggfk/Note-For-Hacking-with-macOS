c3a1305707381b1e0e3fae3d46b3e9f5bb7dd603
Jason Wang <wyqdgggfk@aol.com>
2020-04-14 21:20:12 +0800

    create project08

---
+++ Chapter 08/Project8/Project8/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Jason on 2020/4/14.
+//  Copyright © 2020 jiangrenandu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
--- Project08 Odd One Out.md
+++ Project08 Odd One Out.md
@@ -1,4 +1,4 @@
-**Project 08**
+# **Project 08**
 
 Odd One Out
 
