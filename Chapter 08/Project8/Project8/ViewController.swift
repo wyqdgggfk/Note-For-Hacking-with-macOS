9f106728bd3df44dcded417e96b95d70b76ae0df
Jason Wang <wyqdgggfk@aol.com>
2020-04-14 22:53:34 +0800

    Update ViewController.swift

--- Chapter 08/Project8/Project8/ViewController.swift
+++ Chapter 08/Project8/Project8/ViewController.swift
@@ -21,6 +21,25 @@ class ViewController: NSViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func loadView() {
+        super.loadView()
+        
+        visualEffectView = NSVisualEffectView()
+        visualEffectView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // enable the dark vibrancy effect
+        visualEffectView.material = .dark
+        
+        // force it to remain active even when the window loses focus
+        visualEffectView.state = .active
+        view.addSubview(visualEffectView)
+        
+        //pin it to the edges of our window
+        visualEffectView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        visualEffectView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        visualEffectView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        visualEffectView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
