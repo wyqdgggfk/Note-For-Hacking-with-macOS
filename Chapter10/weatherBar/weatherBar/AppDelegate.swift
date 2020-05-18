21378226c12893c724484f89207b3348a5534a36
Jason Wang <wyqdgggfk@aol.com>
2020-05-18 22:40:10 +0800

    another weatherBar App

---
+++ Chapter10/weatherBar/weatherBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  weatherBar
+//
+//  Created by Jason on 2020/5/18.
+//  Copyright © 2020 jiangrenandu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
