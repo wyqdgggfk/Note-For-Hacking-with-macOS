217584dd85737af6bbeb3bb4495e0042eff2d3b4
Jason Wang <wyqdgggfk@aol.com>
2020-05-22 22:13:48 +0800

    create Project_01

---
+++ Resourse/Project01/Project_01/Project_01/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project_01
+//
+//  Created by Jason on 2020/5/22.
+//  Copyright © 2020 jiangrenandu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
