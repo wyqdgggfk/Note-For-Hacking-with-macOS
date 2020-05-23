8393525e230751392734d57455712a8c7d8dbe72
Jason Wang <wyqdgggfk@aol.com>
2020-05-23 17:10:54 +0800

    load image

--- Resourse/Project01/Project_01/Project_01/SourceViewController.swift
+++ Resourse/Project01/Project_01/Project_01/SourceViewController.swift
@@ -10,20 +10,30 @@ import Cocoa
 
 class SourceViewController: NSViewController,NSTableViewDataSource,NSTableViewDelegate {
     @IBOutlet weak var tableView: NSTableView!
+    var pictures = [String]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
     }
     // How many rows will show up
     func numberOfRows(in tableView: NSTableView) -> Int {
         return 100
     }
+    
     // what will be shown in every cell
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
         guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else {return nil}
         vw.textField?.stringValue = "Hello World"
-        
         return vw
     }
     
