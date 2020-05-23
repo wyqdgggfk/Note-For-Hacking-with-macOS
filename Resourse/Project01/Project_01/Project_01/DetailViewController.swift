2e5cfc0b7b259f9fa9ceb6a0c8c5ac3e74adc211
Jason Wang <wyqdgggfk@aol.com>
2020-05-23 13:23:20 +0800

    create tableview and imageview outlet

---
+++ Resourse/Project01/Project_01/Project_01/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  Project_01
+//
+//  Created by Jason on 2020/5/23.
+//  Copyright © 2020 jiangrenandu. All rights reserved.
+//
+
+import Cocoa
+
+class DetailViewController: NSViewController {
+    @IBOutlet weak var imageView: NSImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ Resourse/Project01/Project_01/Project_01/SourceViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SourceViewController.swift
+//  Project_01
+//
+//  Created by Jason on 2020/5/23.
+//  Copyright © 2020 jiangrenandu. All rights reserved.
+//
+
+import Cocoa
+
+class SourceViewController: NSViewController {
+    @IBOutlet weak var tableView: NSTableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
