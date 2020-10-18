32a3177ee113d2821911eade3eaf648c9cb06e7c
Jason Wang <wyqdgggfk@aol.com>
2020-10-18 18:27:11 +0800

    update

---
+++ Chapter01/Project1/Project1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Project1
+//
+//  Created by Jason on 2020/10/18.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Chapter01/Project1/Project1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Jason on 2020/10/18.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
